a8047dd5b81590bcfdf12529d5bd3fb547f6f7b6
Mykyta Karpin <mkarpin@mirantis.com>
2018-12-15 22:08:04 +0200

    Add more detailed analysis

--- drafts/gerrit.py
+++ drafts/gerrit.py
@@ -5,12 +5,15 @@ import json
 from pprint import pprint
 import time
 from datetime import timedelta, date
+from collections import OrderedDict
+import matplotlib.pyplot as plt
+from operator import itemgetter
 
 def daterange(start_date, end_date):
     for n in range(int ((end_date - start_date).days)):
         yield start_date + timedelta(n)
 
-start = (2018,12,01)
+start = (2018,12,11)
 end = (2018,12,15)
 
 d_start = date(start[0],start[1],start[2])
@@ -22,35 +25,73 @@ print d_end
 client = GerritSSHClient("review")
 
 #projects = ['nova']
-projects=['nova','cinder','glance','keystone','horizon','neutron','designate','heat','ironic','barbican', 'aodh', 'ceilometer', 'gnocchi', 'panko', 'manila', 'salt', 'linux', 'reclass', 'galera', 'memcached', 'rabbitmq', 'bind', 'apache', 'runtest', 'oslo-templates', 'auditd', 'octavia', 'openscap']
+openstack_projects = ['nova','cinder','glance','keystone','horizon','neutron','designate','heat','ironic','barbican', 'aodh', 'ceilometer', 'gnocchi', 'panko', 'manila', 'salt', 'linux', 'reclass', 'galera', 'memcached', 'rabbitmq', 'bind', 'apache', 'runtest', 'oslo-templates', 'auditd', 'octavia', 'openscap']
+openstack_salt_formulas = ['salt-formulas/{}'.format(i) for i in openstack_projects]
+pkg_projects = ['^packaging/specs/.*']
+model_projects = ['mk/cookiecutter-templates','salt-models/reclass-system']
+projects = openstack_salt_formulas + pkg_projects + model_projects
 data = {}
-stats = {'sum_patch_sets': 0}
-
+stats = {}
+days = []
 
 
 for i in projects:
-    cmd = "query --patch-sets --format=JSON project:salt-formulas/{} after:{} before:{}".format(i,d_start,d_end)
+    cmd = "query --patch-sets --format=JSON project:{} after:{}".format(i,d_start)
     result = client.run_gerrit_command(cmd)
     data[i] = [json.loads(change) for change in result.stdout.read().rstrip().split('\n')]
-    stats[i] = {'sum_patch_sets': 0}
-
+    stats[i] = OrderedDict()
 
 for d in daterange(d_start, d_end):
     day = d.strftime("%Y-%m-%d")
+    days.append(day)
     ut_start = time.mktime(d.timetuple())
     ut_end = time.mktime((d+timedelta(days=1)).timetuple())
     for k,v in data.items():
-        stats[k][day] = {}
-        stats[k][day]['patch_sets'] = 0
+        stats[k][day] = 0
         for change in v:
             if change.get('patchSets'):
                 for p in change['patchSets']:
                     if p['createdOn'] >= ut_start and p['createdOn'] <= ut_end:
-                        stats[k][day]['patch_sets'] += 1
-        stats[k]['sum_patch_sets'] += stats[k][day]['patch_sets']
+                        stats[k][day] += 1
 
-for k,v in data.items():
-    print stats[k]['sum_patch_sets']
-    stats['sum_patch_sets'] += stats[k]['sum_patch_sets']
+# Patch sets per day over all projects
+stats_days = OrderedDict()
+period_sum = 0
+for d in days:
+    stats_days[d] = 0
+    for project,s in stats.items():
+        stats_days[d] += s[d]
+    period_sum += stats_days[d]
+    print 'Day {} - patch_sets {}'.format(d,stats_days[d])
+print 'Number of patch_sets for period {} - {} is {}'.format(d_start,d_end,period_sum)
 
-print stats['sum_patch_sets']
+# Patch sets per project
+for i in projects:
+    print 'Project {}:'.format(i)
+    days_sum = 0
+    for d in days:
+        days_sum += stats[i][d]
+        if stats[i][d] > 0:
+            print '    {} - patch_sets {}'.format(d,stats[i][d])
+    if days_sum > 0:
+        print '    Period patch_sets {}'.format(days_sum)
+    else:
+        print 'No patch_sets found for the period'
+
+
+# Find days with the highest number of patch sets
+# and check in which project it was
+x = sorted(stats_days.items(), key=itemgetter(1))
+max_ps = x[-1][1]
+max_days = []
+for i in x:
+    if max_ps in i:
+        max_days.append(i[0])
+for i in projects:
+    for d in max_days:
+        print '{} - {} - patch_sets {}'.format(i,d,stats[i][d])
+
+
+
+#plt.plot(stats.keys(),stats.values())
+#plt.show()
