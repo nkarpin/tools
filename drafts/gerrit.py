b06e2c2bd52fb425d67367ce213b4bedf7216ebe
Mykyta Karpin <mkarpin@mirantis.com>
2018-12-16 14:04:49 +0200

    Add args, per-project, max per projects group statistics

--- drafts/gerrit.py
+++ drafts/gerrit.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+from __future__ import division
 from pygerrit.ssh import GerritSSHClient
 import json
 from pprint import pprint
@@ -8,28 +8,35 @@ from datetime import timedelta, date
 from collections import OrderedDict
 import matplotlib.pyplot as plt
 from operator import itemgetter
+import argparse
 
 def daterange(start_date, end_date):
     for n in range(int ((end_date - start_date).days)):
         yield start_date + timedelta(n)
 
-start = (2018,12,11)
-end = (2018,12,15)
+parser = argparse.ArgumentParser(description='')
+parser.add_argument('--start', help='start of the period in format yyyy-mm-dd')
+parser.add_argument('--end', help='end of the period in format yyyy-mm-dd')
+parser.add_argument('--max', action="store_true", default=False , help='Show day with maximum number of patch sets overall projects')
+parser.add_argument('--per-project', action="store_true", dest='per_project', default=False , help='Show statistics per each project')
+
+args = parser.parse_args()
+
+start = [int(i) for i in args.start.split('-')]
+end = [int(i) for i in args.end.split('-')]
 
 d_start = date(start[0],start[1],start[2])
 d_end = date(end[0],end[1],end[2])
 
-print d_start
-print d_end
-
 client = GerritSSHClient("review")
 
-#projects = ['nova']
 openstack_projects = ['nova','cinder','glance','keystone','horizon','neutron','designate','heat','ironic','barbican', 'aodh', 'ceilometer', 'gnocchi', 'panko', 'manila', 'salt', 'linux', 'reclass', 'galera', 'memcached', 'rabbitmq', 'bind', 'apache', 'runtest', 'oslo-templates', 'auditd', 'octavia', 'openscap']
 openstack_salt_formulas = ['salt-formulas/{}'.format(i) for i in openstack_projects]
 pkg_projects = ['^packaging/specs/.*']
 model_projects = ['mk/cookiecutter-templates','salt-models/reclass-system']
 projects = openstack_salt_formulas + pkg_projects + model_projects
+#projects = ['salt-formulas/nova']
+
 data = {}
 stats = {}
 days = []
@@ -54,42 +61,89 @@ for d in daterange(d_start, d_end):
                     if p['createdOn'] >= ut_start and p['createdOn'] <= ut_end:
                         stats[k][day] += 1
 
+stat0 = """
+
+Statistics about patch sets overall projects for the period:
+"""
+
+print stat0
+
 # Patch sets per day over all projects
 stats_days = OrderedDict()
 period_sum = 0
+apd = 0
+mpd = 0
+aio_ds = 0
+m_ds = 0
 for d in days:
     stats_days[d] = 0
-    for project,s in stats.items():
-        stats_days[d] += s[d]
+    for p in projects:
+        if stats[p][d] > 0:
+            if p in openstack_salt_formulas + pkg_projects:
+                apd += 1
+                aio_ds += stats[p][d]
+            else:
+                mpd += 1
+                m_ds += stats[p][d]
+        stats_days[d] += stats[p][d]
     period_sum += stats_days[d]
-    print 'Day {} - patch_sets {}'.format(d,stats_days[d])
-print 'Number of patch_sets for period {} - {} is {}'.format(d_start,d_end,period_sum)
+    print '    Day {} - patch_sets {}'.format(d,stats_days[d])
+print '    Total in ALL projects {} - {} is {}'.format(d_start,d_end,period_sum)
 
-# Patch sets per project
-for i in projects:
-    print 'Project {}:'.format(i)
-    days_sum = 0
-    for d in days:
-        days_sum += stats[i][d]
-        if stats[i][d] > 0:
-            print '    {} - patch_sets {}'.format(d,stats[i][d])
-    if days_sum > 0:
-        print '    Period patch_sets {}'.format(days_sum)
-    else:
-        print 'No patch_sets found for the period'
+if aio_ds > 0:
+    print '    Total in formulas + packages {}'.format(aio_ds)
+    print '    Mean per day in formulas + packages {}'.format(aio_ds / apd)
+if m_ds > 0:
+    print '    Total in models projects {}'.format(m_ds)
+    print '    Mean per day in models projects {}'.format(m_ds / mpd)  
 
 
-# Find days with the highest number of patch sets
-# and check in which project it was
-x = sorted(stats_days.items(), key=itemgetter(1))
-max_ps = x[-1][1]
-max_days = []
-for i in x:
-    if max_ps in i:
-        max_days.append(i[0])
-for i in projects:
-    for d in max_days:
-        print '{} - {} - patch_sets {}'.format(i,d,stats[i][d])
+if args.per_project:
+    stat1 = """
+
+Statistics about patch sets per project for the period:
+"""
+
+    print stat1
+
+    # Patch sets per project
+    for i in projects:
+        print 'Project {}:'.format(i)
+        days_sum = 0
+        patch_days = 0
+        for d in days:
+            days_sum += stats[i][d]
+            if stats[i][d] > 0:
+                patch_days += 1
+                print '    {} - patch_sets {}'.format(d,stats[i][d])
+        if days_sum > 0:
+            mean = days_sum / patch_days
+            print '    Total patch_sets {}'.format(days_sum)
+            print '    Mean per day {}'.format(mean)
+        else:
+            print '    No patch_sets found for the period'
+
+
+if args.max:
+    stat2 = """
+
+Statistics about day with higest number of patchsets overall projects:
+"""
+
+    print stat2
+
+
+    # Find days with the highest number of patch sets
+    # and check in which project it was
+    x = sorted(stats_days.items(), key=itemgetter(1))
+    max_ps = x[-1][1]
+    max_days = []
+    for i in x:
+        if max_ps in i:
+            max_days.append(i[0])
+    for i in projects:
+        for d in max_days:
+            print '    {} - {} - patch_sets {}'.format(i,d,stats[i][d])
 
 
 
