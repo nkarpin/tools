28b4f9f7fa73c34621523a7eb16eb9553e7f0c8b
Mykyta Karpin <mkarpin@mirantis.com>
2020-03-06 17:22:25 +0200

    new logs

--- drafts/logs_downloader.py
+++ drafts/logs_downloader.py
@@ -12,33 +12,39 @@ parser.add_argument('url', help='url to get logs from')
 args = parser.parse_args()
 url = args.url
 
-cluster_name = url.split('/')[-2]
+cluster_name = url.split('/')[-3]
 logs_path = '/tmp/%s' % (cluster_name)
 
 r = requests.get(url)
 soup = BeautifulSoup(r.content,'html5lib')
-links = soup.findAll('a')
+all_links = soup.findAll('a')
 
-node_links = [url + link['href'] for link in links if '../' not in link['href']]
-nodes = {}
+links = [url + link['href'] for link in all_links if '../' not in link['href']]
+dirs = {cluster_name: []}
 
-#print node_links
+print links
 
-for l in node_links:
-#    print l
-    lk = BeautifulSoup(requests.get(l).content,'html5lib').findAll('a')
-#    print lk
-    nodes[l.split('/')[-2]] = [l + link['href'] for link in lk if not '../' in link['href']]
+for l in links:
+    lk = []
+    if l.endswith('/'):
+        print "Found Dir! {}".format(l)
+        lk = BeautifulSoup(requests.get(l).content,'html5lib').findAll('a')
+        dirs[l.split('/')[-2]] = [l + link['href'] for link in lk if not '../' in link['href']]
+    else:
+        print "Found File! {}".format(l)
+        dirs[cluster_name].append(l)
+print dirs
 
 if not os.path.exists(logs_path):
+    print "Creating {}".format(logs_path)
     os.makedirs(logs_path)
 
-for n,links in nodes.iteritems():
+for n,lks in dirs.iteritems():
     node_logs_path = "%s/%s" % (logs_path,n)
     if not os.path.exists(node_logs_path):
         os.makedirs(node_logs_path)
-#    print links
-    for l in links:
+    print lks
+    for l in lks:
         f_name = l.split('/')[-1]
         log = requests.get(l)
         log_name = "%s/%s" % (node_logs_path,f_name) 
@@ -50,5 +56,5 @@ for n,links in nodes.iteritems():
         if f_name.endswith('.tar.gz'):
             with tarfile.open(log_name,"r") as tar_ref:
                 tar_ref.extractall("%s/" % str(node_logs_path))
-        os.remove(log_name)
+        #os.remove(log_name)
 
